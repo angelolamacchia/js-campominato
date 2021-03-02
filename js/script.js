58aed569349504b7ac78e1cd2d29121e3d210a30
Angelo Lamacchia <angelo.lamacchia@live.it>
2021-03-02 15:59:53 +0100

    modificata logica e aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -1,52 +1,78 @@
-/*Il computer deve generare 16 numeri casuali tra 1 e 100.
-I numeri non possono essere duplicati
-In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-L’utente non può inserire più volte lo stesso numero.
-Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.*/
-alert("Benvenuto nel campo minato per giocare premi Ok")
+/*BONUS: (da fare solo se funziona tutto il resto)
+all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+con difficoltà 0 => tra 1 e 100
+con difficoltà 1 =>  tra 1 e 80
+con difficoltà 2 => tra 1 e 50*/
+alert("Benvenuto nel campo minato per giocare premi Ok");
 
 //elenco numeri casuali
-var arrayNumeri = [];
+var arrayBombe = [];
+//numero bombe modificabile
+const numeroBombe = 16;
+
+var max = 100;
+
+//elenco numeri utente
+var arrayNumeriUt = [];
+
+//l'utente decide la difficoltà
+do {
+    var difficoltàUt = prompt("Inserire difficolta: 0 per Facile, 1 per Medio, 2 per difficile");
+    console.log(difficoltàUt);
+} while (difficoltàUt > 2)
+
+if (difficoltàUt == 0) {
+    alert("Hai scelto la difficoltà Facile")
+} else if (difficoltàUt == 1) {
+    alert("Hai scelto la difficoltà Medio")
+    max = 80;
+} else if (difficoltàUt == 2) {
+    alert("Hai scelto la difficoltà Difficile")
+    max = 50;
+}
 
 //genero e controllo che i numeri siano diversi
 var i = 0;
 do {
-    var numeroCasuale = numeroRandom(1,100);
-    if (presente(arrayNumeri,numeroCasuale)==false) {
-        arrayNumeri.push(numeroCasuale);
+    var numeroCasuale = numeroRandom(1,max);
+    if (presente(arrayBombe,numeroCasuale)==false) {
+        arrayBombe.push(numeroCasuale);
         i++;
     }   
-} while (arrayNumeri.length < 16);
+} while (arrayBombe.length < numeroBombe);
 
-console.log(arrayNumeri);
+console.log(arrayBombe);
 
-//elenco numeri utente
-var arrayNumeriUt = [];
+//flag per interrompere il ciclo
+var partitaPersa = false; 
+
+while (arrayNumeriUt.length < (max - numeroBombe) && partitaPersa==false) {
+    partitaPersa = false;
+    do {
+        //variabile numero utente
+        var numeroUt = parseInt( prompt("Inserire numero da 1 a " + max));
+        if ((isNaN(numeroUt)) || (numeroUt < 1) || (numeroUt > max)) {
+            alert("Inserire valore valido")
+        }
+    } while ((isNaN(numeroUt)) || (numeroUt < 1) || (numeroUt > max))
     
-while (arrayNumeriUt.length < 84) {
-    //variabile numero utente
-    var numeroUt = parseInt( prompt("Inserire numero da 1 a 100"));
-
     if (presente(arrayNumeriUt,numeroUt)==false) {
         //inserisco il numero utente nell'array
         arrayNumeriUt.push(numeroUt);
-        document.getElementById("numeri").innerHTML = numeroUt;
     } else if (presente(arrayNumeriUt,numeroUt)==true) {
-        alert("Il numero è stato utilizzato o errato");
+        //verifico che il numero non è stato utilizzato
+        alert("Il numero è stato utilizzato");
     }
-    console.log(arrayNumeriUt);
     
-    if (presente(arrayNumeri,numeroUt) ==true) {
+    if (presente(arrayBombe,numeroUt)==true) {
         alert("Boom hai preso il numero sbagliato");
         alert("Il tuo punteggio è: " + arrayNumeriUt.length);
-        alert("Ritenti?");
-        location.reload();
-        break; 
-     } //else if (presente(arrayNumeri,numeroUt) ==false) {
-    //     alert("Sei fortunato il numero non era nell'elenco");
-    // }
+        // alert("Ritenti?");
+        // location.reload();
+        partitaPersa = true;
+    }
+
+     console.log(arrayNumeriUt);
 }
 
 
